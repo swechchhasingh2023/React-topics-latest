032a5d81c7e903a50f6e1cffc7826d9510d1cd5f
Swechchha Singh <swechchhasingh2023@gmail.com>
2025-09-26 13:01:27 +0530

    Counter app with Toolkit

---
+++ CounterReduxToolkit/src/App.jsx
@@ -0,0 +1,12 @@
+import CounterOperation from "./components/CounterOperation";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <CounterOperation />
+    </>
+  );
+}
+
+export default App;
